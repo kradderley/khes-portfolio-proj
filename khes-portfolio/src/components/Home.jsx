cedb58c5277e28e5ae381505a9ea0edb9efb3612
Khes Adderley <adderleyrkhes@gmail.com>
2024-01-08 11:25:09 -0500

    homepage-in-progress

---
+++ khes-portfolio/src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Khes Adderley</h1>
+      <h2>Junior Web Developer</h2>
+      <p>
+        I am currently seeking employment as a Junior Web Developer in an
+        organization that allows me the opportunity to apply my technical
+        skills, while offering the room for growth and professional development.
+      </p>
+      <button>
+        Say Hello
+        <img src="/send.svg" alt="Send" />
+      </button>
+    </>
+  );
+};
+
+export default Home;
