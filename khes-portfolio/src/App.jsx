0f0e2faa8e96ecdc694c9c8fd51ca43d6be88647
Khes Adderley <adderleyrkhes@gmail.com>
2024-01-12 11:56:18 -0500

    responsive e lements started

--- khes-portfolio/src/App.jsx
+++ khes-portfolio/src/App.jsx
@@ -3,14 +3,20 @@ import Menu from "./components/Menu";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 const App = () => {
   return (
+    // <>
+    //   <Menu />
+    //   <Home />
+    //   <About />
+    //   <Skills />
+    //   <Projects />
+    // </>
     <>
-      <Menu />
       <Home />
       <About />
-      <Skills />
     </>
   );
 };
--- khes-portfolio/src/components/Home.jsx
+++ khes-portfolio/src/components/Home.jsx
@@ -21,22 +21,22 @@ const Home = () => {
         </section>
         <section>
           <div>
-            <h1>Khes Adderley</h1>
-            <h2>Junior Web Developer</h2>
-            <p>
+            <h1 className="home-heading">Khes Adderley</h1>
+            <h2 className="home-role-heading">Junior Web Developer</h2>
+            <p className="home-statement">
               I am currently seeking employment as a Junior Web Developer in an
-              organization that allows me the opportunity to apply my technical
+              organization that allows me showcase my technical
               skills, while offering the room for growth and professional
               development.
             </p>
             <br />
-            <button>
-              Say Hello
-              <img className="send" src="../src/images/send.svg" alt="Send" />
-            </button>
+            <div className="home-button-container">
+              <button className="home-button">
+                Say Hello
+                <img className="send" src="../src/images/send.svg" alt="Send" />
+              </button>
+            </div>
           </div>
-          <br />
-          <p>Tech Stack</p>
         </section>
         <section>
           <div>
@@ -44,6 +44,20 @@ const Home = () => {
               className="headshot"
               src="../src/images/khes-resume-headshot.png"
               alt="headshot"
+            />
+          </div>
+        </section>
+        <section>
+        <div className="social-media-container">
+            <img
+              className="social-media-icon"
+              src="../src/images/linkedin.svg"
+              alt="linkedin"
+            />
+            <img
+              className="social-media-icon"
+              src="../src/images/instagram.svg"
+              alt="instagram"
             />
           </div>
         </section>
