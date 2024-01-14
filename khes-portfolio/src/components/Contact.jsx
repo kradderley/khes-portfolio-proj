ae62f04f97da01ed27715b29366e89b42383f16c
Khes Adderley <adderleyrkhes@gmail.com>
2024-01-13 19:43:14 -0500

    responsive added +

---
+++ khes-portfolio/src/components/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../css-files/About.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="contact-container">
+        <h1 className="contact-heading">Contact</h1>
+        <h3 className="contact-subheading">Let's Get In Touch</h3>
+        <div className="contact-grid-container">
+          <section className="contact-item">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <div>Email</div>
+            <div>khesadderley@gmail.com</div>
+            <div>Write Me </div>
+          </section>
+          <section className="contact-item">
+            <FontAwesomeIcon icon={faPhone} />
+            <div>Phone</div>
+            <div>+33 7 88 11 94 40</div>
+            <div>Send me a message </div>
+          </section>
+          <section></section>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
--- khes-portfolio/src/components/Projects.jsx
+++ khes-portfolio/src/components/Projects.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import "../css-files/About.css";
 
 const Projects = () => {
   return (
     <>
-      <div>
-        <h1>Projects</h1>
-        <h3>Most Recent Work </h3>
-        <div>
+      <div className="projects-container">
+        <h1 className="projects-heading">Projects</h1>
+        <h3 className="projects-subheading">Most Recent Work </h3>
+        <div className="projects-grid-container">
           <section>
             <h4>Weather App</h4>
-            <ul>
+            <ul className="projects-list">
               <li>HTML</li>
               <li>CSS</li>
               <li>Javacript</li>
@@ -17,14 +18,14 @@ const Projects = () => {
           </section>
 
           <section>
-            <h4>La Recette - Recipe Website</h4>
-            <ul>
+            <h4>La Recette: Recipe Website</h4>
+            <ul className="projects-list">
               <li>React</li>
             </ul>
           </section>
           <section>
             <h4>My Calculator</h4>
-            <ul>
+            <ul className="projects-list">
               <li>React</li>
               <li>Redux</li>
             </ul>
